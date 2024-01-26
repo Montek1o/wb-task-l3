f91191718a7b9e055e012f9f7380b674bbf99287
Andrey Nezhdanov <nezhd.an@yandex.ru>
2024-01-27 00:17:14 +0300

    feat: доделаны заголовки UserID в запросах на получение товаров

--- src/index.ts
+++ src/index.ts
@@ -3,10 +3,16 @@ import Router from "./router";
 import { cartService } from "./services/cart.service";
 import { userService } from "./services/user.service";
 
-new Router();
-cartService.init();
-userService.init();
+async function app() {
+  cartService.init();
+  await userService.init();
 
-setTimeout(() => {
-  document.body.classList.add("is__ready");
-}, 250);
+  const route = new Router();
+  route.route();
+
+  setTimeout(() => {
+    document.body.classList.add("is__ready");
+  }, 250);
+}
+
+app();
